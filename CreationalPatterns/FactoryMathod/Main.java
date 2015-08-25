4b48ac1a799aca799e1b68fc975dcd3bb6f78fdb
mplatini <mychel.platini2@gmail.com>
2015-08-24 23:10:35 -0300

    Added class

--- CreationalPatterns/FactoryMathod/Main.java
+++ CreationalPatterns/FactoryMathod/Main.java
@@ -1,6 +1,8 @@
-public static void main(String[] args){
-	
-	System.out.println("Teste");
-	
+public class Main{
 
-}
+	public static void main(String[] args){
+	
+		System.out.println("Teste");
+	
+ }
+	}
